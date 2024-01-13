52f2680756b8f132abad22bd5016c3022c373a1f
MYOUSUFFS <mailyousuffsend@gmail.com>
2024-01-13 11:38:20 +0530

    billdesk

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+
+import 'billdesk_class.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Flutter BillDesk demo"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('BillDesk payment intergration'),
+            Text('BillDesk', style: Theme.of(context).textTheme.headlineMedium),
+            Container(
+              child: Lottie.asset("json/json.json"),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => MyBillDesk(),
+        tooltip: 'Increment',
+        label: Text(
+          "Pay",
+          style: TextStyle(fontSize: 18),
+        ),
+        icon: Icon(Icons.send),
+      ),
+    );
+  }
+}
